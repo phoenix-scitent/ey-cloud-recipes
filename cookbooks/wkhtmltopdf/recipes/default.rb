6de20e21e629066a78b94b84626eaa9e26d6daa0
eglassman <eglassman@scitent.com>
2012-10-19 11:43:35 -0400

    updates wkhtmltopdf for 64bit

--- cookbooks/wkhtmltopdf/recipes/default.rb
+++ cookbooks/wkhtmltopdf/recipes/default.rb
@@ -14,11 +14,11 @@ package "x11-libs/libXrender" do
 end
 execute "wkhtmltopdf" do
   command %Q{
-    wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.9.9-static-i386.tar.bz2 
-    tar xvjf wkhtmltopdf-0.9.9-static-i386.tar.bz2
-    sudo mv wkhtmltopdf-i386 /usr/local/bin/wkhtmltopdf
+    wget http://wkhtmltopdf.googlecode.com/files/wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
+    tar xvjf wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
+    sudo mv wkhtmltopdf-amd64 /usr/local/bin/wkhtmltopdf
     sudo chmod +x /usr/local/bin/wkhtmltopdf
-    rm wkhtmltopdf-0.9.9-static-i386.tar.bz2
+    rm wkhtmltopdf-0.10.0_rc2-static-amd64.tar.bz2
   }
   not_if { FileTest.exists?("/usr/local/bin/wkhtmltopdf") }
 end
