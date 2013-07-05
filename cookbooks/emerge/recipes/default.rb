5176118f44a32c07280aa424643aad6cef2d5923
Eric Glassman <eglassman@scitent.com>
2013-07-05 01:32:03 -0400

    updates imagemagick dependencies

--- cookbooks/emerge/recipes/default.rb
+++ cookbooks/emerge/recipes/default.rb
@@ -23,3 +23,17 @@
 #   version "2.7.6"
 #   action :install
 # end
+
+# Imagemagick and dependencies via engineyard prebuilt binary
+enable_package "media-libs/lcms" do
+  version "2.3"
+end
+
+enable_package "media-gfx/imagemagick" do
+  version "6.7.8.8-r1"
+end
+
+package "media-gfx/imagemagick" do
+  version "6.7.8.8-r1"
+  action :install
+end
