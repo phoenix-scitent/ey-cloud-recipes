44469261d480b2594728d9e42f170092893df731
Eric Glassman <eglassman@scitent.com>
2014-07-02 14:08:31 -0400

    moves script to a different location

--- cookbooks/clone_scripts/recipes/default.rb
+++ cookbooks/clone_scripts/recipes/default.rb
@@ -3,11 +3,6 @@
 # Recipe:: default
 #
 
-execute "install-fog-to system" do
-  command "gem install nokogiri -v 1.5.11 && gem install  fog -v 1.19.0"
-  not_if "gem list fog"
-end
-
 include_recipe "clone_scripts::database"
 include_recipe "clone_scripts::deploy"
 include_recipe "clone_scripts::rsync"
