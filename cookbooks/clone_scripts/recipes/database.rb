cc5bac983f459ecf8823f15cbf38a13b4f764f07
eric glassman <eglassman@scitent.com>
2014-08-05 11:02:24 -0400

    updates recipe to allow for choice of specific day

--- cookbooks/clone_scripts/recipes/database.rb
+++ cookbooks/clone_scripts/recipes/database.rb
@@ -4,15 +4,15 @@
 #
 
 if ['db_slave'].include?(node[:instance_role])
-  
+
   execute "install-fog-to system" do
     command "gem install nokogiri -v 1.5.11 && gem install  fog -v 1.19.0"
     not_if "gem list | grep fog"
   end
-  
+
   node['applications'].each do |app_name,data|
-    if node['environment']['name'] == node['clone_scripts']['source_environment_name']    
-      # This script will download the latest eybackup then uses s3_backup.rb to upload it to the 
+    if node['environment']['name'] == node['clone_scripts']['source_environment_name']
+      # This script will download the latest eybackup then uses s3_backup.rb to upload it to the
       # S3 region and bucket specified in the attribute
       template "/home/#{node['owner_name']}/database_backup.sh"do
         source "database_backup.sh.erb"
@@ -26,7 +26,7 @@ if ['db_slave'].include?(node[:instance_role])
           :backup_ext => node['clone_scripts']['database']['backup_ext']
         })
       end
-    
+
       # This script is called by database_backup.sh.  It will upload the eybackup to S3.
       template "/home/#{node['owner_name']}/s3_backup.rb"do
         source 's3.rb.erb'
@@ -42,20 +42,20 @@ if ['db_slave'].include?(node[:instance_role])
           :upload => true
         })
       end
-    
+
       cron "dump-and-upload-database-for-#{app_name}" do
         action  :create
         minute  "0"
         hour    node['clone_scripts']['database']['backup_hour']
-        day     "*"
+        day     node['clone_scripts']['database']['day']
         month   "*"
         weekday "*"
         command "/home/#{node['owner_name']}/database_backup.sh"
         user    "root"
-      end  
-    end    
-    
-    # Scripts to be rendered on D/R environment only.  database_restore.sh calls s3_restore.rb, 
+      end
+    end
+
+    # Scripts to be rendered on D/R environment only.  database_restore.sh calls s3_restore.rb,
     # it will download the latest eybackup and then import it.
     if node['environment']['name'] == node['clone_scripts']['clone_environment_name']
       template "/home/#{node['owner_name']}/database_restore.sh"do
@@ -70,7 +70,7 @@ if ['db_slave'].include?(node[:instance_role])
           :backup_ext => node['clone_scripts']['database']['backup_ext']
         })
       end
-  
+
       template "/home/#{node['owner_name']}/s3_restore.rb"do
         source 's3.rb.erb'
         owner node['owner_name']
@@ -89,19 +89,18 @@ if ['db_slave'].include?(node[:instance_role])
         action  :create
         minute  "0"
         hour    node['clone_scripts']['database']['restore_hour']
-        day     "*"
+        day     node['clone_scripts']['database']['day']
         month   "*"
         weekday "*"
         command "/home/#{node['owner_name']}/database_restore.sh"
         user    "root"
-      end      
-    end 
+      end
+    end
   end
-  
+
   # Create the directory used to store the backups
   directory "/mnt/tmp/" do
     action :create
   end
-        
-end   
 
+end
