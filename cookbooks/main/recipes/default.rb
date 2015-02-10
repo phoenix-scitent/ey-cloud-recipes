5d48cea155069dba04c25b6bd20d1fde7f7aceb9
eric glassman <eglassman@scitent.com>
2015-02-10 16:14:01 -0500

    updates to add active papertrail recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -101,6 +101,9 @@ require_recipe "clone_scripts"
 # uncomment to include the PHP recipe
 # require_recipe "php"
 
+#uncomment to include the papertrail recipe
+include_recipe "papertrail"
+
 #enable Extension modules for a given Postgresql database
 # if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # Extensions that support both Postgres 9.0, 9.1 and 9.2
