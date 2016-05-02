94dd43921e5c1211a5bd03544dca1958ea72dc5b
Jonathan Marion <jmarion@scitent.com>
2016-05-02 00:24:36 -0400

    more updates and hopefully the last

--- cookbooks/clone_scripts/attributes/default.rb
+++ cookbooks/clone_scripts/attributes/default.rb
@@ -4,11 +4,11 @@ clone_scripts(
   'weekday_to_run' => "1",                                           # The day of week at which the full script occurs, "0" is Sunday (0-6)
   'database' => {
     :backup_bucket => "scitent-clone-51041190d661",                  # A random bucket name to store the latest db dump for the clone
-    :backup_hour => "4",                                             # The hour at which the latest db dump should be uploaded to S3 (UTC)
-    :restore_hour => "4"                                             # The hour at which the latest db dump should be downloaded from S3 and imported (UTC)
+    :backup_hour => "5",                                             # The hour at which the latest db dump should be uploaded to S3 (UTC)
+    :restore_hour => "6"                                             # The hour at which the latest db dump should be downloaded from S3 and imported (UTC)
   },
   'deploy' => {
-    :hour => "4",                                                    # The hour at which the automated deploy should happen (UTC)
+    :hour => "5",                                                    # The hour at which the automated deploy should happen (UTC)
     :branch => "master-data-test"                                    # The branch to deploy for the automated deploy
   },
   'rsync' => {
