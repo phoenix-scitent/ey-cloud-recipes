942f052f63d6590136188c02ac1298ed0e128d96
Jonathan Marion <jmarion@scitent.com>
2016-05-15 04:02:59 -0400

    updates more

--- cookbooks/clone_scripts/attributes/default.rb
+++ cookbooks/clone_scripts/attributes/default.rb
@@ -1,18 +1,18 @@
 clone_scripts(
   'source_environment_name' => "aha_prod",                           # The source environment name
   'clone_environment_name' => "aha_clone",                           # The cloned environment name
-  'weekday_to_run' => "5",                                           # The day of week at which the full script occurs, "0" is Sunday (0-6)
+  'weekday_to_run' => "0",                                           # The day of week at which the full script occurs, "0" is Sunday (0-6)
   'database' => {
     :backup_bucket => "scitent-clone-51041190d661",                  # A random bucket name to store the latest db dump for the clone
-    :backup_hour => "1",                                             # The hour at which the latest db dump should be uploaded to S3 (UTC)
-    :restore_hour => "1"                                             # The hour at which the latest db dump should be downloaded from S3 and imported (UTC)
+    :backup_hour => "9",                                             # The hour at which the latest db dump should be uploaded to S3 (UTC)
+    :restore_hour => "10"                                             # The hour at which the latest db dump should be downloaded from S3 and imported (UTC)
   },
   'deploy' => {
-    :hour => "1",                                                    # The hour at which the automated deploy should happen (UTC)
+    :hour => "10",                                                    # The hour at which the automated deploy should happen (UTC)
     :branch => "master-data-test"                                    # The branch to deploy for the automated deploy
   },
   'rsync' => {
-    :hour => "1",                                                    # The hour at which to begin the rsync (UTC)
+    :hour => "9",                                                    # The hour at which to begin the rsync (UTC)
     :source_hostname => "ec2-54-243-138-55.compute-1.amazonaws.com", # The hostname of the app_master in the source env
   }
 )
