e396640282ef035a1467c61070e197f1fe91c2cf
Jonathan Marion <jmarion@scitent.com>
2017-07-31 11:48:58 -0400

    AHADEV-263 moving v4 recipe 'emerge' to v5 recipes

---
+++ cookbooks/emerge/recipes/default.rb
@@ -0,0 +1,39 @@
+#############################################
+# Sample recipe for emerging packages
+#
+# Search the Engine Yard portage tree to find
+# out package versions to install
+#
+# EXAMPLE:
+#
+# Ensure local package index is synced with tree
+# $ eix-sync
+#
+# Search for libxml2
+# $ eix libxml2
+#############################################
+
+# Unmask version 2.7.6 of libxml2
+# enable_package "dev-libs/libxml2" do
+#   version "2.7.6"
+# end
+
+# Install the newly unmasked version
+# package "dev-libs/libxml2" do
+#   version "2.7.6"
+#   action :install
+# end
+
+# Imagemagick and dependencies via engineyard prebuilt binary
+enable_package "media-libs/lcms" do
+  version "2.3"
+end
+
+enable_package "media-gfx/imagemagick" do
+  version "6.7.9.9"
+end
+
+package "media-gfx/imagemagick" do
+  version "6.7.9.9"
+  action :install
+end
